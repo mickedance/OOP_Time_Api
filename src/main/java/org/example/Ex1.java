2c66451c551b05f66c93cae26e516d1d0a205578
mickedance <mickedance@hotmail.com>
2022-04-11 11:56:35 +0200

    Initial commit + Ex1

---
+++ src/main/java/org/example/Ex1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Ex1 {
+
+    /**
+     * Printing out todays date in ISO Format
+     * @param args
+     */
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+
+        System.out.println(currentDate);
+    }
+}
