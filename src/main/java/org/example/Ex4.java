73e8afa0ff59cd76efc8f8ea6f632f33c982bbe2
mickedance <mickedance@hotmail.com>
2022-04-11 14:01:22 +0200

    Ex4

---
+++ src/main/java/org/example/Ex4.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Ex4 {
+    /**
+     *         //Create a LocalDate object from a String by using the .parse() method.
+     * @param args
+     */
+    public static void main(String[] args) {
+        String s = "1984-06-06";
+        LocalDate date = LocalDate.parse(s);
+        System.out.println(date);
+    }
+}
